824e2f8ae07cf865f20cf09f11b2d16268e7752c
semenov-vldr <semenov-vldr@users.noreply.github.com>
2024-07-12 20:49:47 +0000

    deploy: d258e129538aeb6ba40e14ec766e8113408ecaf2

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -342,6 +342,35 @@ phoneInputs.forEach(function (input) {
 });
 "use strict";
 
+new Swiper('.books__slider', {
+  navigation: {
+    prevEl: '.swiper-prev',
+    nextEl: '.swiper-next'
+  },
+  // Откл функционала, если слайдов меньше, чем нужно
+  watchOverflow: true,
+  loop: true,
+  // Стартовый слайд
+  initialSlide: 0,
+  // Брейк поинты (адаптив)
+  // Ширина экрана
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 16
+    },
+    370: {
+      slidesPerView: 3,
+      spaceBetween: 16
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  }
+});
+"use strict";
+
 function mobileNav() {
   var header = document.querySelector("header.header");
   if (!header) return;
